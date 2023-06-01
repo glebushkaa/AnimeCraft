a798799f83175977122a70dd6bb272b4abe2dc46
glebushkaa <gleb.mokryy@gmail.com>
2023-06-01 16:41:38 +0300

    ktlint changes

--- app/src/main/java/ua/anime/animecraft/ui/screens/settings/components/LanguageDropDown.kt
+++ app/src/main/java/ua/anime/animecraft/ui/screens/settings/components/LanguageDropDown.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
