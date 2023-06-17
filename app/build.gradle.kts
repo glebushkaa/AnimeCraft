25cd00d363ea745d8ab48a1f5726783a8c28ba45
glebushkaa <gleb.mokryy@gmail.com>
2023-06-17 05:17:55 +0300

    fixed several bugs from release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,10 +23,16 @@ android {
         applicationId = "ua.anime.animecraft"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.0.0"
 
         vectorDrawables.useSupportLibrary = true
+    }
+
+    bundle {
+        language {
+            enableSplit = false
+        }
     }
 
     buildTypes {
