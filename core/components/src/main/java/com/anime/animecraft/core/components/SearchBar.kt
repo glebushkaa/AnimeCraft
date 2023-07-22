7a954349cb57d3f1907b8bf4b293922b4c5f8f11
glebushkaa <gleb.mokryy@gmail.com>
2023-07-22 12:50:17 +0300

    solved ktlint prob

--- core/components/src/main/java/com/anime/animecraft/core/components/SearchBar.kt
+++ core/components/src/main/java/com/anime/animecraft/core/components/SearchBar.kt
@@ -20,8 +20,8 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import ua.anime.animecraft.core.components.R
 import com.anime.animecraft.core.theme.theme.AppTheme
+import ua.anime.animecraft.core.components.R
 
 /**
  * Created by gle.bushkaa email(gleb.mokryy@gmail.com) on 5/7/2023.
